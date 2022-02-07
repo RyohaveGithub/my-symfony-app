f6692bd8e0b2a9b037bc0614f05abd3ed20d3656
RyoUchida0415 <tkkndshr2565@gmail.com>
2022-02-07 23:00:25 +0900

    コントローラの作成方法とアノテーションのルート設方法を勉強

---
+++ src/Controller/HelloController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HelloController 
+{
+   /**
+     * @Route("/hello",name="hello")
+     */
+
+    public function index()
+    {
+        $result = <<< EOM
+        <html>
+        <head><title>Hello</title></head>
+        <body>
+        <h1>Hello Symfony!</h1>
+        <p>this is symfomy sample page.</p>
+        </body>
+        </html>
+    EOM;
+        return new Response($result);
+    }
+}
