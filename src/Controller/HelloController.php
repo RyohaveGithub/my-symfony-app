959e67809307ca6dcfe6768aa690fed36aa58c9a
RyoUchida0415 <tkkndshr2565@gmail.com>
2022-02-13 22:23:48 +0900

    フォームの送信の勉強

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -2,40 +2,39 @@
 
 namespace App\Controller;
 
-use Psr\Log\LoggerInterface;
+// use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\JsonResponse;
+// use Symfony\Component\HttpFoundation\Response;
+// use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
+
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class HelloController extends AbstractController
 {
    /**
-     * @Route("/hello/{msg}",name="hello")
+     * @Route("/hello",name="hello")
      */
 
-    public function index($msg='hello')
+    public function index(Request $request)
     {
-      return $this->render('hello/index.html.twig',[
-        'controller' => 'HelloController', 
-        'action' => 'index',
-        'pre_action'=>'(none)',
-        'message'=> $msg ,
-      ]); 
-    }
+        $form =$this->createFormBuilder()
+            ->add('input', TextType::class)
+            ->add('save',  SubmitType::class,['label'=>'Click'])
+            ->getForm();
 
-    /**
-     * @Route("/other/{action}/{msg}",name="other")
-     */
-
-    public function other($action, $msg)
-    {
+        if($request->getMethod()== 'POST'){
+            $form -> handleRequest($request);
+            $msg = 'こんにちは'.$form->get('input')->getData().'さん!';
+        }else{
+            $msg = 'お名前をどうぞ!';
+        }
       return $this->render('hello/index.html.twig',[
-        'controller' => 'HelloController',  
-        'action' => 'other',
-        'pre_action'=>$action,
-        'message'=> $msg ,
+        'title' => 'hello', 
+        'message' => $msg,
+        'form'=>$form->createView(),
       ]); 
     }
 }
